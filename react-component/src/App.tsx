6f883e1e8027dde01fafd25ef4636fdc24fea53f
seois830926 <idmahoro@gmail.com>
2022-07-11 22:34:38 +0900

    getValue사용시 값이 잘못나오는 현상 수정 -> 입력손봐야함

--- react-component/src/App.tsx
+++ react-component/src/App.tsx
@@ -5,56 +5,43 @@ import CustomButton from './components/button/CustomButton';
 import CustomInput from './components/input/CustomInput';
 
 class App extends Component {
+  cardNoInput: any;
+  regNoInput: any;
+  regNoMaskInput: any;
 
   getValue = () => {
-    alert('getValue');
+    console.log(this.cardNoInput.getValue());
+    console.log(this.cardNoInput.getOriginValue());
+    console.log(this.regNoInput.getValue());
+    console.log(this.regNoInput.getOriginValue());
+    console.log(this.regNoMaskInput.getValue());
+    console.log(this.regNoMaskInput.getOriginValue());
   }
   
   render() {
    /**
-    * 1. 클릭
-    * 2. store로 getValue전송
-    * 3. store에서 각 input의 value를 가져옴
-    * 4. 각 input은 store에 연결되있어야 함
+    * 1. 클릭(완)
+    * 2. 각 input에 ref부착
+    * 3. 클릭시 각 input에서 value값 가져옴
     */
     return (
      <div>
        <CustomButton classProps="fa fa-google" label='etValue' onClick={this.getValue}></CustomButton>
        <div>
          <label>카드번호: </label>
-         <CustomInput classProps="default" mask='LLLL-LLLL-LLLL-LLLL'></CustomInput>
+         <CustomInput ref={(ref) => (this.cardNoInput = ref)} classProps="default" mask='LLLL-LLLL-LLLL-LLLL'></CustomInput>
        </div>
        <div>
          <label>주민등록번호: </label>
-         <CustomInput classProps="default" mask='LLLLLL-LLLLLLL'></CustomInput>
+         <CustomInput ref={(ref) => (this.regNoInput = ref)} classProps="default" mask='LLLLLL-LLLLLLL'></CustomInput>
        </div>
        <div>
          <label>주민등록번호: </label>
-         <CustomInput classProps="default" mask='LLLLLL-L******'></CustomInput>
+         <CustomInput ref={(ref) => (this.regNoMaskInput = ref)} classProps="default" mask='LLLLLL-L******'></CustomInput>
        </div>
      </div>
     )
    };
 }
-
-// function App() {
-//   return (
-//     <div>
-//       <CustomButton classProps="fa fa-google" label='etValue'></CustomButton>
-//       <div>
-//         <label>카드번호: </label>
-//         <CustomInput classProps="default" mask='LLLL-LLLL-LLLL-LLLL'></CustomInput>
-//       </div>
-//       <div>
-//         <label>주민등록번호: </label>
-//         <CustomInput classProps="default" mask='LLLLLL-LLLLLLL'></CustomInput>
-//       </div>
-//       <div>
-//         <label>주민등록번호: </label>
-//         <CustomInput classProps="default" mask='LLLLLL-L******'></CustomInput>
-//       </div>
-//     </div>
-//   );
-// }
 
 export default App;
